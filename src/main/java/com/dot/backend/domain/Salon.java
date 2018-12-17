ef5e7b436af42dbb3ddf2222c6495d35b8998fd2
Amit Kumar <fbpass.res14@gmail.com>
2018-12-17 22:31:52 +0530

    Return salon gender type in response

--- src/main/java/com/dot/backend/domain/Salon.java
+++ src/main/java/com/dot/backend/domain/Salon.java
@@ -13,13 +13,15 @@ public class Salon {
     private String name;
     private String address;
     private String image;
+    private String gender_type;
     @DBRef
     private List<Category> categories;
 
-    public Salon(String name, String address, String image) {
+    public Salon(String name, String address, String image, String gender_type) {
         this.name = name;
         this.address = address;
         this.image = image;
+        this.gender_type = gender_type;
     }
 
     public String getId() {
@@ -60,5 +62,13 @@ public class Salon {
 
     public void setCategories(List<Category> categories) {
         this.categories = categories;
+    }
+
+    public String getGender_type() {
+        return gender_type;
+    }
+
+    public void setGender_type(String gender_type) {
+        this.gender_type = gender_type;
     }
 }
